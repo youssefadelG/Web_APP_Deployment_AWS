41df9dd06f1a0504bf5192351462ddee3d30e5e2
Sameh Tawfik <58302710+sameh-Tawfiq@users.noreply.github.com>
2024-08-21 10:07:53 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 
+from flask import Flask
+from app.routes.user_routes import user_blueprint
+from app.routes.product_routes import product_blueprint
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register blueprints
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(product_blueprint)
+
+    return app
