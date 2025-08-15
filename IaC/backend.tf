267963ac11642768c8f932ef61eb98c63e122c82
youssefadelG <youssefadel99@gmail.com>
2025-08-15 10:46:28 +0300

    changed backend to bucket in new env

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tfstate-730335400401"
+    bucket = "s3-tfstate-590184052750"
     key    = "web-app-deployment/terraform.tfstate"
     region = "us-east-1"
   }
